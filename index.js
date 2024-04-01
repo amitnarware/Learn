d6ec177444070eed15b04a96df9fc37a06332395
amitnarware <amitnarware40@gmail.com>
2024-04-01 22:00:29 +0530

    res.send change

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express();
 
 app.use('/', function(req,res){
-      res.send('hello i am amit')
+      res.send('hello i am ankush')
 })
 
 app.listen(3000,() => {
