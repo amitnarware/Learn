6f1108e9102008d1d99f084497330efdfacdc142
amitnarware <amitnarware40@gmail.com>
2024-04-01 21:56:28 +0530

    first-commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const app = express();
+
+app.use('/', function(req,res){
+      res.send('hello i am amit')
+})
+
+app.listen(3000,() => {
+      console.log('server is running on 3000')
+})
