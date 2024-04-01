18cdb188db04718f4d1cf7eb0300ef260d23647e
amitnarware <amitnarware40@gmail.com>
2024-04-02 00:11:46 +0530

    want merge

--- app.js
+++ app.js
@@ -4,7 +4,7 @@
       <meta charset="UTF-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <title>learn-github</title>
-      <h1> hello i am amit currently i am learning git hub</h1>
+      <h1> hello i am amit currently i am learning git hub and create a branch </h1>
 </head>
 <body>
       
